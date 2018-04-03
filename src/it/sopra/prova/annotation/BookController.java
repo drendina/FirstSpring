7d4385b5615b1d847dfc64f5e54464dfe0c8d154
RENDINA Daniele <daniele.rendina@soprasteria.com>
2018-04-03 15:27:09 +0200

    version 1

--- src/it/sopra/prova/annotation/BookController.java
+++ src/it/sopra/prova/annotation/BookController.java
@@ -13,46 +13,38 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import it.sopra.prova.dto.Book;
 
-
 @Controller
 public class BookController {
 
-  private List<Book> bookList;
-	
-  public BookController(){
-    bookList = new ArrayList<Book>();
-  }
-	
-  // new comment 1 alterate 0011223344
-  
-  //Mostra la pagina di creazione 
-  @RequestMapping(value = "/createBook", method = RequestMethod.GET)
-  public String welcome(Model model) {
-    model.addAttribute("book",new Book());
-    return "/book/createBook";
-  }
-	
-  //controlla se ci sono errori di validazione e in caso contrario aggiunge il libro alla lista
-  @RequestMapping(value = "/create", method = RequestMethod.POST)
-  public String create(@Valid Book book, BindingResult result) {
-    if(result.hasErrors()){
-      return "/book/createBook";
-    }
-    bookList.add(book);
-    return "redirect:getview";
-  }
-	
-  //mostra la lista dei libri
- // @RequestMapping(value = "/getview")
-  //public String view(Model model) {
-   // model.addAttribute("bookList",bookList);
-   // return "/book/view";
-  //}
-  
-  @RequestMapping(value = "/getview")
-  public String view1(Model model) {
-    model.addAttribute("bookList",bookList);
-    return "/book/view1";
-  }
-  
+	private List<Book> bookList;
+
+	public BookController() {
+		bookList = new ArrayList<Book>();
+	}
+
+	// Mostra la pagina di creazione
+	@RequestMapping(value = "/createBook", method = RequestMethod.GET)
+	public String welcome(Model model) {
+		model.addAttribute("book", new Book());
+		return "/book/createBook";
+	}
+
+	// controlla se ci sono errori di validazione e in caso contrario aggiunge
+	// il libro alla lista
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public String create(@Valid Book book, BindingResult result) {
+		if (result.hasErrors()) {
+			return "/book/createBook";
+		}
+		bookList.add(book);
+		return "redirect:getview";
+	}
+
+	// mostra la lista dei libri
+	@RequestMapping(value = "/getview")
+	public String view(Model model) {
+		model.addAttribute("bookList", bookList);
+		return "/book/view";
+	}
+
 }
